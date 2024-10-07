629746b583105eb095568c19d5580570d2e50411
rua-iri <rory.mcguigan@protonmail.com>
2024-10-07 16:00:35 +0300

    add: card component

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,38 @@
-import { CountButton } from "~features/count-button"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+
 
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="h-80 w-56">
-      <CountButton />
-    </div>
+    <>
+      <Card className="max-w-[500px]">
+        <CardHeader>
+          <CardTitle>ScriptBlaster</CardTitle>
+          <CardDescription>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            aliquid consectetur doloribus sit labore, cum sint ratione commodi,
+            blanditiis suscipit animi repellendus esse ipsum vero quas delectus
+            perspiciatis. Pariatur, modi quo quia accusamus ex vero at eveniet
+            officiis! Illum voluptatum beatae eligendi possimus molestiae enim
+            dolores corporis aliquid adipisci accusamus.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </>
   )
 }
 
